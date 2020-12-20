7442ecbecf1c3be8a032c78ab41863a9c30b862e
filipeas <felipealvessampaio@hotmail.com>
2020-12-20 11:46:03 -0300

    bug na atividade F

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
  * TUTORIAL:
  *
  * Opcoes no teclado
- * 0 - DESENHAR RETA
- * 1 - DESENHAR TRIANGULO
- * 2 - DESENHAR QUADRILATERO
- * 3 - RESENHAR POLIGONO
- * 4 - DESENHAR CIRCUNFERENCIA
+ * 0 - DESENHAR RETA (atividade A e B)
+ * 1 - DESENHAR TRIANGULO (atividade d)
+ * 2 - DESENHAR QUADRILATERO (atividade c)
+ * 3 - RESENHAR POLIGONO (atividade E e H)
+ * 4 - DESENHAR CIRCUNFERENCIA (atividade G)
  * 5 - MUDAR COR PARA PRETO
  * 6 - MUDAR COR PARA VERMELHO
  * 7 - MUDAR COR PARA VERDE
  * 8 - MUDAR COR PARA AZUL
- * 9 - ATIVAR FLOOD FILL
- * - - LIMPAR TELA
+ * 9 - ATIVAR FLOOD FILL (atividade I)
+ * - - LIMPAR TELA (não funciona...)
  * ESC - FECHA PROGRAMA
  *
  * Opcoes no mause:
@@ -20,19 +20,23 @@
  *
  * Observacoes:
  * 1) Para desenhar eh necessario selecionar a forma desejada, e depois clicar
- * na tela para desenha-lo. Caso seja uma reta ou quadrilatero eh preciso 2
- * cliques.
+ * na tela com o botao esquerdo para desenha-lo. Caso seja uma reta ou quadrilatero
+ *  eh preciso 2 cliques.
  * Se for um triangulo, 3 cliques. E se for um poligono, eh possivel desenhar a
  * partir de 3 cliques e logo depois clique com o botao direito para fechar
- * o poligono.
+ * o poligono automaticamente.
  * 2) Para colorir uma figura, primeiro selecione uma cor e depois selecione a
  * opcao de flood fill.  Depois basta clicar com o botao esquerdo dentro da
- * figura e ele ira automaticamente preenche-la. Tem um possivel bug no preenchimento
+ * figura e ele ira automaticamente preenche-la. Ha bug no preenchimento
  * que acontece quando eh diminuido a largura da linha. Depois de alguns testes
- * foi fixado com tamanho 3, mas ainda pode ocorrer do floodfill encontrar uma
+ * foi fixado com tamanho 5, mas ainda pode ocorrer do floodfill encontrar uma
  * brecha na figura e desenhar o fundo da tela. Se isso ocorrer ele ficara em loop
  * desenhando o fundo e nunca vai acabar.
- *
+ * Tomar cuidade para usar o flood fill no fundo branco da tela. Se fizer isso
+ * o programa entrará em loop pois ele vai tentar preencher o branco do fundo
+ * da tela.
+ * 3) Não foi implementado a atividade F. Não consegui usar os pontos selecionados
+ * e aplicar as transformacoes apos um desenho ser feito.
  */
 
 
@@ -221,25 +225,25 @@ void mouse(int button, int state, int x, int y)
 void display(void)
 {
 	cor1 = 0, cor2 = 0, cor3 = 0;
-	glColor3f (0.0, 0.0, 0.0); // menu preto
+	// glColor3f (0.0, 0.0, 0.0); // menu preto
 	glClear(GL_COLOR_BUFFER_BIT); // Limpa o Buffer de Cores
 
 	// barra de menu
-	glBegin(GL_QUADS);
-	glVertex2f(200, 1000);
-	glVertex2f(0, 1000);
-	glVertex2f(0, 0);
-	glVertex2f(200, 0);
-	glEnd();
+	// glBegin(GL_QUADS);
+	// glVertex2f(200, 1000);
+	// glVertex2f(0, 1000);
+	// glVertex2f(0, 0);
+	// glVertex2f(200, 0);
+	// glEnd();
 
 	glColor3f (1.0, 1.0, 1.0); // quadro branco
 	// quadro de desenho
-	glBegin(GL_QUADS);
-	glVertex2f(2000, 1000);
-	glVertex2f(150, 1000);
-	glVertex2f(150, 0);
-	glVertex2f(2000, 0);
-	glEnd();
+	// glBegin(GL_QUADS);
+	// glVertex2f(2000, 1000);
+	// glVertex2f(150, 1000);
+	// glVertex2f(150, 0);
+	// glVertex2f(2000, 0);
+	// glEnd();
 
 	glPointSize(5);
 
@@ -361,179 +365,4 @@ void drawPontos()
 		pnt = pnt->prox;
 	}
 	glEnd();  // indica o fim do desenho
-}
-
-// Funcao que realiza translacao de uma figura (atividade f))
-void translationElement(double *x, double *y, double tx, double ty)
-{
-	double matriz[3][3] =
-	{
-		{1, 0, 0},
-		{0, 1, 0},
-		{tx, ty, 1}
-	};
-	double k[3] = {*x, *y, 1};
-	double aux = 0, result[3];
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		aux = 0;
-		for(j = 0; j < 3; j++)
-		{
-			aux += k[j] * matriz[j][i];
-		}
-		result[i] = aux;
-	}
-
-	*x = result[0];
-	*y = result[1];
-}
-
-// Funcao que realiza translacao de uma escala (atividade f))
-void scaleElement(double *x, double *y, double sx, double sy)
-{
-	double matriz[3][3] =
-	{
-		{sx, 0, 0},
-		{0, sy, 0},
-		{0, 0, 1}
-	};
-	double k[3] = {*x, *y, 1};
-	double aux = 0, result[3];
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		aux = 0;
-		for(j = 0; j < 3; j++)
-		{
-			aux += k[j] * matriz[j][i];
-		}
-		result[i] = aux;
-	}
-
-	*x = result[0];
-	*y = result[1];
-}
-
-// Funcao que realiza cisalhamento de uma figura (atividade f))
-void shearElement(double *x, double *y, double C, double direcao) //direcao 1 - eixo x 2- eixo y
-{
-	double matriz[3][3];
-	double k[3] = {*x, *y, 1};
-	double aux = 0, result[3];
-	int i, j;
-
-	if (direcao == 1)
-	{
-		int matriz[3][3] =
-		{
-			{1, 0, 0},
-			{C, 1, 0},
-			{0, 0, 1}
-		};
-	}
-	else if(direcao == 2)
-	{
-		int matriz[3][3] =
-		{
-			{1, C, 0},
-			{0, 1, 0},
-			{0, 0, 1}
-		};
-	}
-
-	for (i = 0; i < 3; i++)
-	{
-		aux = 0;
-		for(j = 0; j < 3; j++)
-		{
-			aux += k[j] * matriz[j][i];
-		}
-		result[i] = aux;
-	}
-	*x = result[0];
-	*y = result[1];
-}
-
-// Funcao que realiza refrexao de uma figura (atividade f))
-void reflectionElement(double *x, double *y, int direcao) //direcao 1 - eixo x 2- eixo y 3-origem
-{
-	double matriz[3][3];
-	double k[3] = {*x, *y, 1};
-	double aux = 0, result[3];
-	int i, j;
-
-	if (direcao == 1)
-	{
-		double	matriz[3][3] =
-		{
-			{1, 0, 0},
-			{0, -1, 0},
-			{0, 0, 1}
-		};
-	}
-	else if(direcao == 2)
-	{
-		double	matriz[3][3] =
-		{
-			{ -1, 0, 0},
-			{0, 1, 0},
-			{0, 0, 1}
-		};
-	}
-	else if(direcao == 3)
-	{
-		double matriz[3][3] =
-		{
-			{ -1, 0, 0},
-			{0, -1, 0},
-			{0, 0, 1}
-		};
-	}
-
-	for (i = 0; i < 3; i++)
-	{
-		aux = 0;
-		for(j = 0; j < 3; j++)
-		{
-			aux += k[j] * matriz[j][i];
-		}
-		result[i] = aux;
-	}
-
-	*x = result[0];
-	*y = result[1];
-}
-
-// Funcao que realiza rotacao de uma figura (atividade f))
-void rotarionElement(double *x, double *y, double grau)
-{
-	int k[3] = {*x, *y, 1};
-	double cosseno, seno;
-	cosseno = cos( grau * M_PI / 180.0 );
-	seno = sin(grau * M_PI / 180.0);
-	double matriz[3][3] =
-	{
-		{cosseno, seno, 0},
-		{ -seno, cosseno, 0},
-		{0, 0, 1}
-	};
-	int aux = 0, result[3];
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		aux = 0;
-		for(j = 0; j < 3; j++)
-		{
-			aux += k[j] * matriz[j][i];
-		}
-		result[i] = aux;
-	}
-
-	*x = result[0];
-	*y = result[1];
-
 }
